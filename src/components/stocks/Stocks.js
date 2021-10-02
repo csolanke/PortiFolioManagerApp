d22acaddf6c41b7d3d17303e8757e0e9df3bf234
Chandrakant <csolanke77@gmail.com>
2021-10-01 22:40:28 -0400

    Refactioring the the code

--- src/components/stocks/Stocks.js
+++ src/components/stocks/Stocks.js
@@ -1,48 +1,47 @@
 import StockItem from './StockItem';
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 
 import Card from '../UI/Card'
 import StockFilter from './StockFilter'
 
 const Stocks = (props) => {
 
-  const [filteredYear , setFilteredYear] = useState('2020');
+  const [filteredYear, setFilteredYear] = useState('2020');
 
-  const filterChangeHandler = (selectedYear)=>{
+  const filterChangeHandler = (selectedYear) => {
     setFilteredYear(selectedYear);
   };
 
   const filteredStocks = props.stocks.filter(
-    st=>{
-    
+    st => {
       let convertedNum = Number(filteredYear);
-      return st.year=== convertedNum;
+      return st.year === convertedNum;
     }
   );
 
+let stockContent = <p>No Expense Found</p>
 
-console.log(filteredStocks.length);
+if(filteredStocks.length> 0)
+{
+  stockContent = (filteredStocks.map(ab => <StockItem
+    name={ab.name}
+    price={ab.price}
+    day={ab.day}
+    month={ab.month}
+    year={ab.year}
+    key={ab.id}
+    quantity={ab.quantity} />))
+
+}
 
   return (
-   
-   <div>
 
-  
- <Card className="stocks">
-
- <StockFilter selected={filteredYear} onChangeFilter={filterChangeHandler}></StockFilter> 
- 
-{filteredStocks.map(ab => <StockItem
-  name={ab.name}
-  price={ab.price}
-  day={ab.day}
-  month={ab.month}
-  year={ab.year}
-  key={ab.id} 
-  quantity={ab.quantity} />)}
-
-</Card>
-   </div>
+    <div>
+      <Card className="stocks">
+        <StockFilter selected={filteredYear} onChangeFilter={filterChangeHandler}></StockFilter>
+       {stockContent}
+      </Card>
+    </div>
   )
 }
 
