5dd9621c9d946eb527c11dd2870f7af9af030a1e
Chandrakant <csolanke77@gmail.com>
2021-10-06 23:30:44 -0400

    Add Button to do hide and seek for the form

--- src/components/NewStock/NewStock.js
+++ src/components/NewStock/NewStock.js
@@ -1,6 +1,9 @@
+import React ,{useState} from 'react'
 import './NewStock.css'
 import StockForm from './StockForm'
 const NewStock = (props) => {
+
+    const [isEditing ,setIsEditing] = useState(false);
 
     const stockDataSaveHandler=(stockData)=>{
 
@@ -8,10 +11,31 @@ const NewStock = (props) => {
          ...stockData
         }
     props.onNewStock(StockDataFromNewStock);
+    setIsEditing(false);
+    };
+
+    const startEditingHandler=()=>
+    {
+        setIsEditing(true);
+    }
+
+    const stopEditingHandler=()=>{
+        setIsEditing(false);
     }
 
     return <div className="new-stcok">
-       <StockForm onSaveOfStockData={stockDataSaveHandler}></StockForm>
+      
+      {
+          !isEditing && (
+              <div><button onClick={startEditingHandler}>Add New Stock</button></div>
+          )
+      }
+
+      { isEditing && (<StockForm onSaveOfStockData={stockDataSaveHandler}
+      onCancel={stopEditingHandler}></StockForm>)
+
+      }
+       
     </div>
 
 }
--- src/components/NewStock/StockForm.js
+++ src/components/NewStock/StockForm.js
@@ -79,10 +79,11 @@ const StockForm = (props) => {
 
             <div className="new-stock__control">
                 <label>Date</label>
-                <input type='date' min="2019-01-01" step ="2022-12-31" value ={enteredDate} onChange={dateChangeHandler}/>
+                <input type='date'  value ={enteredDate} onChange={dateChangeHandler}/>
             </div>
             <div className="new-stock__actions">
-            <button type="submit">Add to Holdings</button>
+            <button type="button" onClick={props.onCancel}>Cancel</button>
+            <button type="submit">Add to Holding</button>
             </div>
 
         </div>
